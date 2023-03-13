b8238a430087e94b5a382c9742f9e934e28296d7
DManuelR <davidvillaresi@gmail.com>
2023-03-13 12:23:08 +0100

    Update Practica4.1.R

--- Practica4.1.R
+++ Practica4.1.R
@@ -3,3 +3,13 @@ library(tinytex)
 
 x <- rnorm(1000)
 hist(x)
+library(tools)
+
+y <- rnorm(100) #rnorm da valores seudoaleatorios
+hist(y)
+
+set.seed(1) #generador que al activar da valores aleatorios
+rnorm(10)
+
+set.seed(2)
+
