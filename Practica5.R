bc1579868d574e87f8751d1768a64e8669a62b70
DManuelR <davidvillaresi@gmail.com>
2023-03-14 18:51:58 +0100

    Update Practica5.R

--- Practica5.R
+++ Practica5.R
@@ -19,6 +19,25 @@ boxplot(y1, y2)
 # Las pruebas paramétricas y no paramétricas son diferentes en cuanto a las suposiciones que hacen sobre la distribución de los datos y en cuanto a los tipos de datos que pueden analizar.
 
 
+#9- Una relación lineal se refiere a una relación entre dos variables que se puede representar por una línea recta. Por otro lado, una relación monótona se refiere a una relación entre dos variables que sigue un patrón creciente o decreciente, pero no necesariamente lineal.
+# Generar datos para una relación lineal
+set.seed(123)
+horas_de_estudio <- rnorm(100, mean = 20, sd = 5)
+promedio_de_calificacion <- horas_de_estudio * 0.5 + rnorm(100, mean = 70, sd = 10)
+
+# Generar datos para una relación monótona
+set.seed(123)
+horas_de_estudio <- rnorm(100, mean = 20, sd = 5)
+promedio_de_calificacion <- 100 - 1 / (horas_de_estudio + 1) + rnorm(100, mean = 0, sd = 5)
+
+# Graficar las relaciones
+par(mfrow=c(1,2))
+plot(horas_de_estudio, promedio_de_calificacion, main = "Relación lineal")
+plot(horas_de_estudio, promedio_de_calificacion, main = "Relación monótona")
+#El primer gráfico muestra la relación lineal entre las dos variables. Podemos ver que la nube de puntos sigue una línea recta, lo que indica una relación lineal positiva entre las variables. En cambio, el segundo gráfico muestra la relación monótona entre las dos variables. Podemos ver que la nube de puntos no sigue una línea recta, pero sí sigue un patrón creciente general.
+
+
+
 #10- Se suele aplicar la correlación de rangos de Spearman. Con un par de vectores, por ejemplo, utilizamos cor.test(x, y, method = "spearman").
 
 
